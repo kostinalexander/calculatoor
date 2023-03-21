5e0accbb0ac8f1e38a8e985b06bded36cc5906fb
krut_ <egorrak3221@mail.ru>
2023-03-21 19:47:06 +0300

    Начал делать дз

---
+++ src/main/java/pro/sky/calculatoor/CalculatoorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatoorApplication.class, args);
+	}
+
+}
